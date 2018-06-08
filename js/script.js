6ed04c4772464e1971d8bde34afce078e9133326
Jeevan Deep <g.one235@gmail.com>
2018-06-08 09:49:59 -0700

    add changes

---
+++ js/script.js
@@ -0,0 +1,37 @@
+
+$('document').ready(function(){
+	var authorText='';
+	var quotedBy='';
+	function getQuote(){
+	$.ajax({
+		url:'http://api.forismatic.com/api/1.0/',
+		jsonp: 'jsonp',
+		dataType: 'jsonp',
+		data:{
+			method:'getQuote',
+			lang: 'en',
+			format: 'jsonp'
+		},
+		success:function(response){
+			authorText=response.quoteText;
+			//alert(authorText);
+			quotedBy=response.quoteAuthor;
+			$('#author').text(authorText);
+			if(quotedBy){
+				$('#quotedBy').text("Quoted by: " +quotedBy);
+			}else{
+				$('#quotedBy').text("Quoted by unknown ");
+			}
+		}
+	});
+}
+getQuote();
+$('#quote').on('click',function(){
+	getQuote();
+});
+$('#share').on('click',function(){
+	//getQuote();
+	window.open('https://twitter.com/intent/tweet?text=' +encodeURIComponent(authorText +'--unknown by'+quotedBy));
+});
+
+})
